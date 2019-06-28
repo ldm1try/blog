7a64eaf126319f2ab5a656a460967423ace8a6fc
ldm1try <dimm4eg@gmail.com>
2019-06-28 12:28:25 +1000

    Procfile added

--- public/js/app.js
+++ public/js/app.js
@@ -49171,8 +49171,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Users\lesnyakdv\PhpstormProjects\blog\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Users\lesnyakdv\PhpstormProjects\blog\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\OSPanel\domains\localhost\blog\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\OSPanel\domains\localhost\blog\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
